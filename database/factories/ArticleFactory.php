7307f4de29d67343d789b6c4d33a2839a1849316
nikolaychervon <nikolaychervon25@gmail.com>
2022-04-12 13:04:22 +0500

    Реализовал функционал заполнения БД тестовыми данными.

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence(),
+            'slug' => $this->faker->unique()->slug,
+            'short_description' => $this->faker->text(100),
+            'content' => $this->faker->text(500),
+            'published' => $this->faker->boolean(),
+        ];
+    }
+}
